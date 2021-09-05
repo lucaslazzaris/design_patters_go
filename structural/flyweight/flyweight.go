960fd2bddca47a9b0716eac359c64a6919ced0c9
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-05 12:04:48 -0300

    Add simple flyweight

---
+++ structural/flyweight/flyweight.go
@@ -0,0 +1,64 @@
+package main
+
+type Window struct {
+}
+
+type GlyphContext struct {
+}
+
+type BTree struct {
+
+}
+
+type Font struct {
+	index int
+	fonts BTree
+}
+
+type Glyph interface {
+	draw(*Window, GlyphContext)
+
+	setFont(*Font, GlyphContext)
+	getFont(GlyphContext) *Font
+
+	first(GlyphContext)
+	next(GlyphContext)
+	isDone(GlyphContext) bool
+	current(GlyphContext) *Glyph
+
+	insert(*Glyph, GlyphContext)
+	remove(GlyphContext)
+}
+
+// Should implement Glyph interface
+type Character struct {
+	charcode string
+}
+
+type Row struct {
+}
+
+type Column struct {	
+}
+
+type GlyphFactory struct {
+	characters map[string]Character
+}
+
+// See how to verify existence in go
+func (factory *GlyphFactory) createCharacter(char string) *Character {
+	character, ok := factory.characters[char]
+	if(!ok){
+		factory.characters[char] = Character { charcode: char }
+		character = factory.characters[char]
+	}
+	return &character
+}
+
+func (factory *GlyphFactory) createRow(char string) *Row {
+	return new(Row)
+}
+
+func (factory *GlyphFactory) createColumn(char string) *Column {
+	return new(Column)
+}
