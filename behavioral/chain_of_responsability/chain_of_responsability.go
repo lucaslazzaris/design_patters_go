e3ce52d5cb4723447af86440da92fda7e4354d77
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-08 22:12:24 -0300

    Create simple implementation of chain of responsability

---
+++ behavioral/chain_of_responsability/chain_of_responsability.go
@@ -0,0 +1,109 @@
+package main
+
+const (
+	no_help_topic = -1
+	print_topic = 1
+	paper_orientation_topic = 2
+	application_topic = 3
+)
+
+type HelpHandler interface {
+	hasHelp() bool
+	setHandler(HelpHandler)
+	handleHelp()
+}
+
+type ConcreteHelpHandler struct {
+	successor HelpHandler
+	topic int
+}
+
+func (helpHandler *ConcreteHelpHandler) hasHelp() bool {
+	return helpHandler.topic != no_help_topic
+}
+
+func (helpHandler *ConcreteHelpHandler) handleHelp() {
+	if (helpHandler.successor != nil) { 
+		helpHandler.successor.handleHelp()
+	}
+}
+
+// Implements HelpHandler
+type Button struct {
+	successor HelpHandler
+	topic int
+	parent HelpHandler
+}
+
+func (button *Button) hasHelp() bool {
+	return button.topic != no_help_topic
+}
+
+func (button *Button) handleHelp() {
+	if (button.hasHelp()) {
+
+	} else {
+		button.successor.handleHelp()
+	}
+}
+
+func newButton(helpHandler HelpHandler, topic int) *Button {
+	return &Button{topic: topic, parent: helpHandler}
+}
+
+type Dialog struct {
+	successor HelpHandler
+	topic int
+}
+
+func (dialog *Dialog) hasHelp() bool {
+	return dialog.topic != no_help_topic
+}
+
+func (dialog *Dialog) handleHelp() {
+	if (dialog.hasHelp()) {
+
+	} else {
+		dialog.successor.handleHelp()
+	}
+}
+
+func (dialog *Dialog) setHandler(helpHandler HelpHandler) {
+	dialog.successor = helpHandler
+}
+
+func newDialog(helpHandler HelpHandler, topic int) *Dialog {
+	return &Dialog{topic: topic, successor: helpHandler}
+}
+
+type Application struct {
+	successor HelpHandler
+	topic int
+}
+
+func (application *Application) hasHelp() bool {
+	return application.topic != no_help_topic
+}
+
+func (application *Application) handleHelp() {
+	if (application.hasHelp()) {
+
+	} else {
+		application.successor.handleHelp()
+	}
+}
+
+func (application *Application) setHandler(helpHandler HelpHandler) {
+	application.successor = helpHandler
+}
+
+func newApplication(topic int) *Application {
+	return &Application{topic: topic}
+}
+
+func main(){
+	application := newApplication(application_topic)
+	dialog := newDialog(application, print_topic)
+	button := newButton(dialog, paper_orientation_topic)
+	button.handleHelp()
+}
