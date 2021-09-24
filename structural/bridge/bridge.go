6547501fda7385bb7c62175664eff482fcfb6b6b
LucasLazzaris <lucascercal.l@gmail.com>
2021-09-23 21:10:55 -0300

    Improve implementation of bridge

--- structural/bridge/bridge.go
+++ structural/bridge/bridge.go
@@ -1,5 +1,6 @@
 package main
 
+import "fmt"
 type Coord struct {
 	x float32
 	y float32
@@ -21,7 +22,7 @@ type Windower interface {
 	lower()
 	drawLine(Point, Point)
 	drawRect(Point, Point)
-	drawPoligon([]Point)
+	drawPolygon([]Point)
 	drawText(string, Point)
 
 	getWindowImp() WindowImplementer
@@ -31,7 +32,7 @@ type Windower interface {
 type WindowImplementer interface {
 	impTop()
 	impBottom()
-	ImpSetExtent(Point)
+	impSetExtent(Point)
 	impSetOrigin(Point)
 	deviceRect(Coord, Coord, Coord, Coord)
 	deviceText(string, Coord, Coord)
@@ -44,39 +45,121 @@ type ApplicationWindow struct {
 	view *View	
 }
 
-func(window ApplicationWindow) drawRect(point1 Point, point2 Point) {
-	window.windowImp.deviceRect(point1.coord, point2.coord)
+func(window *ApplicationWindow) open() {
+	fmt.Println("Opening")
 }
 
-func (window ApplicationWindow) drawContents() {
+func(window *ApplicationWindow) close() {
+	fmt.Println("Closing")
+}
+
+func(window *ApplicationWindow) iconify() {
+	fmt.Println("Icon")
+}
+
+func(window *ApplicationWindow) deiconify() {
+	fmt.Println("DeIcon")
+}
+
+func(window *ApplicationWindow) setOrigin(point Point) {
+	fmt.Println("Origin", point)
+}
+
+func(window *ApplicationWindow) setExtent(point Point) {
+	fmt.Println("Extent", point)
+}
+
+func(window *ApplicationWindow) raise() {
+	fmt.Println("Raising")
+}
+
+func(window *ApplicationWindow) lower() {
+	fmt.Println("Lower")
+}
+
+func(window *ApplicationWindow) drawLine(p1 Point, p2 Point) {
+	fmt.Println("Drawing Line", p1, p2)
+}
+
+func(window *ApplicationWindow) drawRect(point1 Point, point2 Point) {
+	window.windowImp.deviceRect(*point1.coord, *point1.coord, *point2.coord, *point2.coord)
+}
+
+func(window *ApplicationWindow) drawPolygon(points []Point) {
+	fmt.Println("Drawing Polygon")
+}
+
+func(window *ApplicationWindow) drawText(text string, point Point) {
+	fmt.Println("Drawing Text", text, point)
+}
+
+func (window *ApplicationWindow) drawContents() {
 	window.view.drawOn(window)
 }
 
-func(window ApplicationWindow) getWindowImp() *ApplicationWindowImp {
+func(window *ApplicationWindow) getWindowImp() WindowImplementer {
 	return window.windowImp
 }
 
-type IconWindow struct {
-	windowImp *IconWindowImp
-	view *View	
-	bitmapName string
+func(window *ApplicationWindow) getView() *View {
+	return window.view
 }
 
-func(window IconWindow) drawContents() {
-	if (window.windowImp != nil){
-		window.windowImp.deviceBitmap(window.bitmapName, 0.0, 0.0)
-	}
-}
 
 // ApplicationWindow should implement WindowImplementer
 type ApplicationWindowImp struct {
 
 }
 
-type IconWindowImp struct {
-
+func (imp *ApplicationWindowImp) impTop() {
+	fmt.Println("Top")
 }
+
+func (imp *ApplicationWindowImp) impBottom() {
+	fmt.Println("Bottom")
+}
+
+func (imp *ApplicationWindowImp) impSetExtent(point Point) {
+	fmt.Println("Extent on imp", point)
+}
+
+func (imp *ApplicationWindowImp) impSetOrigin(point Point) {
+	fmt.Println("Origin on imp", point)
+}
+
+func (imp *ApplicationWindowImp) deviceRect(c1 Coord, c2 Coord, c3 Coord, c4 Coord) {
+	fmt.Println("Rect on imp", c1, c2, c3, c4)
+}
+
+
+func (imp *ApplicationWindowImp) deviceText(text string, c1 Coord, c2 Coord) {
+	fmt.Println("Text on imp", text, c1, c2)
+}
+
+func (imp *ApplicationWindowImp) deviceBitmap(text string, c1 Coord, c2 Coord) {
+	fmt.Println("Bitmap on imp", text, c1, c2)
+}
+
+// type IconWindow struct {
+// 	windowImp *IconWindowImp
+// 	view *View	
+// 	bitmapName string
+// }
+
+// func(window IconWindow) drawContents() {
+// 	if (window.windowImp != nil){
+// 		window.windowImp.deviceBitmap(window.bitmapName, 0.0, 0.0)
+// 	}
+// }
+
+// type IconWindowImp struct {
+
+// }
 
 type View struct {
-
 }
+
+func (view *View) drawOn(window Windower) {
+	fmt.Println("Drawing on view", window)
+}
+
